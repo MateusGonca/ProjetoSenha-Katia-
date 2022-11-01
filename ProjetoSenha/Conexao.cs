f119b7e9d911079dff971b53800d2e02f27e1cf1
Aluno <Aluno@LAB06-05>
2022-10-31 21:33:27 -0300

    Adicionar arquivos de projeto.

---
+++ ProjetoSenha/Conexao.cs
@@ -0,0 +1,48 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoSenha
+{
+    public  class Conexao
+    {
+        public MySqlConnection conecta; 
+
+        public void Conectar()
+        {
+            if (conecta.State == ConnectionState.Closed)
+            {
+                conecta.ConnectionString = "Server=localhost;Database=db_senha;User=root;Pwd=cursoads";
+                conecta.Open();
+            }
+        }
+        public void Desconectar()
+        {
+            if (conecta.State == ConnectionState.Open)
+            {
+                conecta.Close();
+            }
+        }
+        public int ExecSql(string sql)
+        {
+
+            int nlinhas;
+            try
+            {
+                MySqlCommand comando = new MySqlCommand();
+                comando.CommandText = sql;
+                comando.Connection = conecta;
+                nlinhas = comando.ExecuteNonQuery();
+            } catch (Exception ex)
+            {
+                Console.WriteLine("Erro: = ex");
+                nlinhas = 0;
+            }
+            return nlinhas;
+        }
+    }
+}
---
+++ ProjetoSenha/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace ProjetoSenha
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnNormal = new System.Windows.Forms.Button();
+            this.btnpPeferencial = new System.Windows.Forms.Button();
+            this.lbSenha = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnNormal
+            // 
+            this.btnNormal.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNormal.Location = new System.Drawing.Point(44, 41);
+            this.btnNormal.Name = "btnNormal";
+            this.btnNormal.Size = new System.Drawing.Size(320, 99);
+            this.btnNormal.TabIndex = 0;
+            this.btnNormal.Text = "Normal";
+            this.btnNormal.UseVisualStyleBackColor = true;
+            this.btnNormal.Click += new System.EventHandler(this.btnNormal_Click);
+            // 
+            // btnpPeferencial
+            // 
+            this.btnpPeferencial.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnpPeferencial.Location = new System.Drawing.Point(44, 199);
+            this.btnpPeferencial.Name = "btnpPeferencial";
+            this.btnpPeferencial.Size = new System.Drawing.Size(320, 99);
+            this.btnpPeferencial.TabIndex = 1;
+            this.btnpPeferencial.Text = "Preferencial";
+            this.btnpPeferencial.UseVisualStyleBackColor = true;
+            this.btnpPeferencial.Click += new System.EventHandler(this.btnpPeferencial_Click);
+            // 
+            // lbSenha
+            // 
+            this.lbSenha.BackColor = System.Drawing.Color.BurlyWood;
+            this.lbSenha.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbSenha.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.lbSenha.Location = new System.Drawing.Point(504, 41);
+            this.lbSenha.Name = "lbSenha";
+            this.lbSenha.Size = new System.Drawing.Size(240, 257);
+            this.lbSenha.TabIndex = 2;
+            this.lbSenha.Text = "Senha";
+            this.lbSenha.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1020, 450);
+            this.Controls.Add(this.lbSenha);
+            this.Controls.Add(this.btnpPeferencial);
+            this.Controls.Add(this.btnNormal);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnNormal;
+        private System.Windows.Forms.Button btnpPeferencial;
+        private System.Windows.Forms.Label lbSenha;
+    }
+}
+
